d3377624b15d6275f0e76b92cc68a777c20f74ce
TACHA1986 <126571508+TACHA1986@users.noreply.github.com>
2023-03-14 22:47:30 +0100

    StringHomework
    Homework (String) is done. To be checked.

---
+++ StringHomework.java
@@ -0,0 +1,24 @@
+package Homework;
+
+public class StringHomework {
+    public static void main(String[] args) {
+
+        String a = "Hello, Java! It's a Java Basic course";
+
+        System.out.println(a);
+
+        // 1. dlina stroki
+        System.out.println(a.length());
+
+        // 2. simvol !
+        System.out.println(a.charAt(11));
+
+        //3. It's a Java
+        System.out.println(a.substring(12,24));
+
+        // 4. Java Basic course
+        System.out.println(a.substring(19));
+
+
+    }
+}
